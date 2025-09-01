e3bad8c714cc0678d7cb8f97b78868bdf7b32740
Peter Kota <kotapeter@gmail.com>
2025-09-01 07:58:16 +0200

    Update response message to version 4.6

--- index2.js
+++ index2.js
@@ -4,7 +4,7 @@ const server = express()
 
 server.get('/', (req, res) => {
   console.log('RUNTIME_ENV', process.env.RUNTIME)
-  res.send('a Hello Kinsta Private 4.5**')
+  res.send('a Hello Kinsta Private 4.6**')
 })
 const port = process.env.PORT || 8080
 server.listen(port, () => {
