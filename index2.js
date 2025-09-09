4b0bfbff2559775711964f1c202dbecd61dab6aa
Peter Kota <kotapeter@gmail.com>
2025-09-09 10:39:38 +0200

    proxy

--- index2.js
+++ index2.js
@@ -6,6 +6,26 @@ server.get('/', (req, res) => {
   console.log('RUNTIME_ENV', process.env.RUNTIME)
   res.send('a Hello Kinsta Private 4.11**')
 })
+server.get('/proxy-99', async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 99_000))
+  res.send('done')
+})
+server.get('/proxy-101', async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 101_000))
+  res.send('done')
+})
+server.get('/proxy-120', async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 120_000))
+  res.send('done')
+})
+server.get('/proxy-90', async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 90_000))
+  res.send('done')
+})
+server.get('/proxy-360', async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 360_000))
+  res.send('done')
+})
 const port = process.env.PORT || 8080
 server.listen(port, () => {
   setInterval(() => {
